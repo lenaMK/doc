af73c3f3f33d71fe56cc28640c29cef386643c49
Lena Krause <lena.krause@umontreal.ca>
2024-04-10 00:01:54 -0400

    ajout compteur pour acu

--- viz/carte/index.js
+++ viz/carte/index.js
@@ -3,9 +3,9 @@ Cartographie des données MONA : œuvres réalisées par des femmes
 LenaMK, Maison MONA, HAR7005
 */
 
-var margin = {top: 20, right: 30, bottom: 30, left: 80},
-width = 1500 - margin.left - margin.right,
-height = 900 - margin.top - margin.bottom;
+var margin = {top: 0, right: 0, bottom: 0, left: 0},
+width = window.innerWidth
+height = window.innerHeight
 
 const svgCarte = d3.select("#carte")
 .attr("width", width + margin.left + margin.right)
@@ -35,7 +35,7 @@ function showToolTip (text, coords){
 }
 
 
-function map (geobaseMTL, outer, pre1982, top50){
+function map (geobaseMTL, outer, pre1982){
 
     proj.center([-73.5878, 45.5088]) // Center on Montreal
         .scale(1000)
@@ -43,22 +43,40 @@ function map (geobaseMTL, outer, pre1982, top50){
 
     path.projection(proj);
 
+    container.selectAll("path")
+        .data(outer.features)
+        .join('path')
+        .attr("d", d => path(d)) 
 
 //layer réseau cyclable MTL
     container.selectAll("path")
         .data(geobaseMTL.features)
         .enter().append("path")
         .attr("d", d => path(d))
-        .attr('stroke', 'lightgrey')
+        .attr('stroke', 'black')
         .attr('fill', 'none')    
 /*
 //layer contour île
-    container.selectAll("path")
-        .data(outer.features)
-        .join('path')
-        .attr("d", d => path(d)) 
 */
-    var dots = container.selectAll("circle");
+
+
+var moreDots = container.selectAll("circle");
+    
+moreDots
+    .data(pre1982)
+    .enter()
+    .append("circle")
+        .attr('class', '50-artwork-dot')
+        .attr("cx", d => proj([d.location.lng, d.location.lat])[0])
+        .attr("cy", d => proj([d.location.lng, d.location.lat])[1])
+        .attr("id", d => d.id)
+        .attr("r", "3px")
+        .attr("fill", '#AA244F')
+        .attr("opacity", 0.5)    
+
+
+
+    var dots = container.selectAll("circles");
 
     dots
         .data(pre1982)
@@ -69,23 +87,10 @@ function map (geobaseMTL, outer, pre1982, top50){
             .attr("cy", d => proj([d.location.lng, d.location.lat])[1])
             .attr("id", d => d.id)
             .attr("r", "1px")
-            .attr("fill", '#AA244F')
-            .attr("opacity", 0.9)    
+            .attr("fill", 'black')
+            .attr("opacity", 1)    
 
-    var moreDots = container.selectAll("circle");
-
-    moreDots
-        .data(top50)
-        .enter()
-        .append("circle")
-            .attr('class', '50-artwork-dot')
-            .attr("cx", d => proj([d.location.lng, d.location.lat])[0])
-            .attr("cy", d => proj([d.location.lng, d.location.lat])[1])
-            .attr("id", d => d.id)
-            .attr("r", "1px")
-            .attr("fill", '#AA244F')
-            .attr("opacity", 0.5)    
-
+    
 
 //zoom sur l'image
     svgCarte.call(
@@ -107,9 +112,20 @@ Promise.all([
     d3.json('../data/Mtl-outer-simplified.json'),
     d3.json('../data/pre1982femx.json'),
     d3.json('../data/top50femx.json'),
-  ]).then(([geobasemtl, outer, pre1982, top50]) => {
-    
-    map(geobasemtl, outer, pre1982, top50);
+  ]).then(([geobasemtl, outer, pre1982]) => {
+    var data = []
+
+    /*
+    augmentation progressive de i pour la séance d'acuponcture de la carte
+    for (var i = 0; i < 18; i++){
+        console.log(i + " " +pre1982[i].title.fr + " " + pre1982[i].artists[0].name + " " + pre1982[i].produced_at)
+        console.log(pre1982[i])
+        data.push(pre1982[i])
+    } 
+       
+    map(geobasemtl, outer, data, top50);
+    */
+    map(geobasemtl, outer, pre1982);
 
   }).catch(function(error) {
     console.log(error);
